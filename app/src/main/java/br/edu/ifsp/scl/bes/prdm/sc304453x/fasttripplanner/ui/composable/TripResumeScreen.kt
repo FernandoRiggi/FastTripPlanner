d641a466ecc7bcf434a2aafb3b97802bee6d1f21
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-29 08:58:56 -0300

    feat: add spacer between the texts in TripResumeScreen

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripResumeScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripResumeScreen.kt
@@ -2,8 +2,10 @@ package br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.composable
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -31,13 +33,22 @@ fun TripResumeScreen(
         modifier = modifier.fillMaxSize().padding(16.dp)
     ) {
         Text("Resumo da Viagem")
+
+        Spacer(modifier = modifier.height(16.dp))
+
         Text("Destino: $destination")
         Text("Número de dias: $days")
         Text("Orçamento diário: R$$dailyBudget")
+
+        Spacer(modifier = modifier.height(16.dp))
+
         Text("Hospedagem: ${accommodationType.label}")
         Text("Transporte: ${if (hasTransport) "Sim" else "Não"}")
         Text("Comida: ${if (hasFood) "Sim" else "Não"}")
         Text("Passeios: ${if(hasTours) "Sim" else "Não"}")
+
+        Spacer(modifier = modifier.height(16.dp))
+
         Text("Total: R$$totalPrice")
 
         Button(modifier = Modifier.fillMaxWidth(),
