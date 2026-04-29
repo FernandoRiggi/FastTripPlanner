d5790da7e29f651167abb69b228b04c1ea99fa22
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-29 09:08:02 -0300

    feat: centralize the resume text and add another column for the other texts in TripResumeScreen

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripResumeScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripResumeScreen.kt
@@ -1,19 +1,20 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.composable
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.AccommodationType
 
 @Composable
@@ -30,33 +31,38 @@ fun TripResumeScreen(
     onRestartClick: () -> Unit
 ) {
     Column(
-        modifier = modifier.fillMaxSize().padding(16.dp)
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Resumo da Viagem")
+        Text("Resumo da Viagem", fontSize = 25.sp, fontWeight = FontWeight.Bold)
 
-        Spacer(modifier = modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Destino: $destination")
-        Text("Número de dias: $days")
-        Text("Orçamento diário: R$$dailyBudget")
-
-        Spacer(modifier = modifier.height(16.dp))
-
-        Text("Hospedagem: ${accommodationType.label}")
-        Text("Transporte: ${if (hasTransport) "Sim" else "Não"}")
-        Text("Comida: ${if (hasFood) "Sim" else "Não"}")
-        Text("Passeios: ${if(hasTours) "Sim" else "Não"}")
-
-        Spacer(modifier = modifier.height(16.dp))
-
-        Text("Total: R$$totalPrice")
-
-        Button(modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                onRestartClick()
-            }
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text("Reiniciar")
+            Text("Destino: $destination")
+            Text("Número de dias: $days")
+            Text("Orçamento diário: R$$dailyBudget")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Hospedagem: ${accommodationType.label}")
+            Text("Transporte: ${if (hasTransport) "Sim" else "Não"}")
+            Text("Alimentação: ${if (hasFood) "Sim" else "Não"}")
+            Text("Passeios: ${if(hasTours) "Sim" else "Não"}")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Total: R$$totalPrice", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+            Button(modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    onRestartClick()
+                }
+            ) {
+                Text("Reiniciar")
+            }
         }
     }
 }
