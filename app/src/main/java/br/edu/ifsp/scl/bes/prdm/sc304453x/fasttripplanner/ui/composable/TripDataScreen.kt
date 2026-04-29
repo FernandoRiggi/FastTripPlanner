b54f6890ea821ecc36426c87edf2a3372441e5c5
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-29 10:13:12 -0300

    style: add title text toTripDataScreen.kt

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripDataScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripDataScreen.kt
@@ -3,8 +3,10 @@ package br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.composable
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -18,8 +20,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.theme.FastTripPlannerTheme
 
 @Composable
@@ -34,13 +38,18 @@ fun TripDataScreen(
     var errorMessage by rememberSaveable {mutableStateOf("") }
 
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
         Column(
-            modifier = modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = modifier.fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
+            Text("Dados da Viagem", fontSize = 26.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(16.dp))
             OutlinedTextField(
                 value = destination,
                 onValueChange = { destination = it },
