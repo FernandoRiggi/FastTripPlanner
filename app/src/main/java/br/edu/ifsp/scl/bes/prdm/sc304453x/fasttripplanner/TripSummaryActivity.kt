e2ffa52f439bad9d0dd3b85facb6cc2e7298acbe
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-05-01 18:54:02 -0300

    feat: send trip data to summary screen

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/TripSummaryActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/TripSummaryActivity.kt
@@ -1,19 +1,63 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+import br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.composable.TripResumeScreen
 import br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.theme.FastTripPlannerTheme
 
 class TripSummaryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val destination = intent.getStringExtra(TripIntentKeys.DESTINATION) ?: ""
+        val days = intent.getIntExtra(TripIntentKeys.DAYS, 0)
+        val dailyBudget = intent.getDoubleExtra(TripIntentKeys.DAILY_BUDGET, 0.0)
+
+        val accommodationName = intent.getStringExtra(TripIntentKeys.ACCOMMODATION)
+            ?: AccommodationType.ECONOMIC.name
+
+        val accommodationType = AccommodationType.valueOf(accommodationName)
+
+        val hasTransport = intent.getBooleanExtra(TripIntentKeys.HAS_TRANSPORT, false)
+        val hasFood = intent.getBooleanExtra(TripIntentKeys.HAS_FOOD, false)
+        val hasTours = intent.getBooleanExtra(TripIntentKeys.HAS_TOURS, false)
+
+        val totalPrice = TripCalculator.calculateTotal(
+            days,
+            dailyBudget,
+            accommodationType,
+            hasTransport,
+            hasFood,
+            hasTours
+        )
+
         enableEdgeToEdge()
         setContent {
             FastTripPlannerTheme {
-                Text("Tela do Resumo da Viagem")
+                TripResumeScreen(
+                    destination = destination,
+                    days = days,
+                    dailyBudget = dailyBudget,
+                    accommodationType = accommodationType,
+                    hasTransport = hasTransport,
+                    hasFood = hasFood,
+                    hasTours = hasTours,
+                    totalPrice = totalPrice,
+                    onRestartClick = {
+                        val restartIntent = Intent(
+                            this@TripSummaryActivity,
+                            MainActivity::class.java
+                        ).apply {
+                            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+
+                        startActivity(restartIntent)
+                        finish()
+                    }
+                )
             }
         }
     }
