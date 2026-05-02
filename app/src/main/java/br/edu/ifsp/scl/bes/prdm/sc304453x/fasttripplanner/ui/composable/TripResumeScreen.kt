f87666ee2ef7af8704a932ccd3a41428eb85bc31
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-05-02 03:42:05 -0300

    style: style: improve trip summary screen layout

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripResumeScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/fasttripplanner/ui/composable/TripResumeScreen.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.composable
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.AccommodationType
+import br.edu.ifsp.scl.bes.prdm.sc304453x.fasttripplanner.ui.theme.FastTripPlannerTheme
 
 @Composable
 fun TripResumeScreen(
@@ -30,38 +34,54 @@ fun TripResumeScreen(
     totalPrice: Double,
     onRestartClick: () -> Unit
 ) {
-    Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Resumo da Viagem", fontSize = 25.sp, fontWeight = FontWeight.Bold)
 
-        Spacer(modifier = Modifier.height(16.dp))
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
 
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                start = 24.dp,
+                end = 24.dp,
+                top = 56.dp,
+                bottom = 24.dp
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Destino: $destination")
-            Text("Número de dias: $days")
-            Text("Orçamento diário: R$$dailyBudget")
+            Text("Resumo da Viagem", fontSize = 25.sp, fontWeight = FontWeight.Bold)
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text("Hospedagem: ${accommodationType.label}")
-            Text("Transporte: ${if (hasTransport) "Sim" else "Não"}")
-            Text("Alimentação: ${if (hasFood) "Sim" else "Não"}")
-            Text("Passeios: ${if(hasTours) "Sim" else "Não"}")
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text("Total: R$$totalPrice", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-
-            Button(modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    onRestartClick()
-                }
+            Column(
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text("Reiniciar")
+                Text("Destino: $destination")
+                Text("Número de dias: $days")
+                Text("Orçamento diário: R$$dailyBudget")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text("Hospedagem: ${accommodationType.label}")
+                Text("Transporte: ${if (hasTransport) "Sim" else "Não"}")
+                Text("Alimentação: ${if (hasFood) "Sim" else "Não"}")
+                Text("Passeios: ${if(hasTours) "Sim" else "Não"}")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text("Total: R$$totalPrice", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        onRestartClick()
+                    }
+                ) {
+                    Text("Reiniciar")
+                }
             }
         }
     }
@@ -70,15 +90,39 @@ fun TripResumeScreen(
 @Preview(showBackground = true)
 @Composable
 fun TripResumeScreenPreview() {
-    TripResumeScreen(
-        destination = "São Paulo",
-        days = 3,
-        dailyBudget = 150.0,
-        accommodationType = AccommodationType.COMFORT,
-        hasTransport = true,
-        hasFood = true,
-        hasTours = false,
-        totalPrice = 1350.0,
-        onRestartClick = {}
-    )
+    FastTripPlannerTheme {
+        TripResumeScreen(
+            destination = "São Paulo",
+            days = 3,
+            dailyBudget = 150.0,
+            accommodationType = AccommodationType.COMFORT,
+            hasTransport = true,
+            hasFood = true,
+            hasTours = false,
+            totalPrice = 1350.0,
+            onRestartClick = {}
+        )
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun TripResumeScreenDarkPreview() {
+    FastTripPlannerTheme {
+        TripResumeScreen(
+            destination = "São Paulo",
+            days = 3,
+            dailyBudget = 150.0,
+            accommodationType = AccommodationType.COMFORT,
+            hasTransport = true,
+            hasFood = true,
+            hasTours = false,
+            totalPrice = 1350.0,
+            onRestartClick = {}
+        )
+    }
 }
